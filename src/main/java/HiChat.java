2ee60472e62f505111efb22dd63d954b9f8b8ec7
Neilchen863 <153107332+Neilchen863@users.noreply.github.com>
2025-01-23 17:07:32 +0800

    Duke Level-3: Mark as Done

--- src/main/java/HiChat.java
+++ src/main/java/HiChat.java
@@ -3,6 +3,37 @@ import java.util.List;
 import java.util.Scanner;
 
 public class HiChat {
+    public static class task{
+        private String task;
+        private boolean isDone;
+
+        public task(String task){
+            this.task = task;
+            this.isDone = false;
+        }
+
+        public void markAsDone(){
+            this.isDone = true;
+        }
+
+        public String getTask(){
+            return this.task;
+        }
+
+        public boolean getIsDone(){
+            return this.isDone;
+        }
+
+        public String toString(){
+            if (this.isDone){
+                return "[X] " + this.task;
+            } else {
+                return "[ ] " + this.task;
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String logo = " __    __   __    ______  __    __       ___   .___________.\n"
@@ -18,11 +49,12 @@ public class HiChat {
 
 
 
-        List<String> listOfTasks = new ArrayList<String>();
+        List<task> listOfTasks = new ArrayList<task>();
 //        String[] listOfTasks = new String[100];
 
         while (true) {
             String command = scanner.nextLine();
+            task newTask = new task(command);
             if (command.equals("bye")) {
                 // Farewell message for "bye"
                 System.out.println("____________________________________________________________\n" +
@@ -41,8 +73,29 @@ public class HiChat {
                 continue;
             }
 
+            if (command.contains("mark") && !command.contains("unmark")) {
+                String[] splitCommand = command.split(" ");
+                int taskNumber = Integer.parseInt(splitCommand[1]);
+                listOfTasks.get(taskNumber - 1).markAsDone();
+                System.out.println("____________________________________________________________\n" +
+                        " Nice! I've marked this task as done:\n" +
+                        "   " + listOfTasks.get(taskNumber - 1) + "\n" +
+                        "____________________________________________________________\n");
+                continue;
+            }
 
-            listOfTasks.add(command);
+            if (command.contains("unmark")) {
+                String[] splitCommand = command.split(" ");
+                int taskNumber = Integer.parseInt(splitCommand[1]);
+                listOfTasks.get(taskNumber - 1).isDone = false;
+                System.out.println("____________________________________________________________\n" +
+                        " OK, I've marked this task as not done yet:\n" +
+                        "   " + listOfTasks.get(taskNumber - 1) + "\n" +
+                        "____________________________________________________________\n");
+                continue;
+            }
+
+            listOfTasks.add(newTask);
             System.out.println("____________________________________________________________\n" +
                     " added: " +
                     command + "\n" +
